a46d91d6e7e1e97cebee12d9776dae972eb0d0c8
mikepakhomov <mpakhomov@griddynamics.com>
2019-04-21 19:03:38 +0200

    getting started exercises: uncurry

--- exercises/src/test/scala/fpinscala/gettingstarted/GettingStartedTests.scala
+++ exercises/src/test/scala/fpinscala/gettingstarted/GettingStartedTests.scala
@@ -1,35 +1,37 @@
 package fpinscala.gettingstarted
 
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.{FunSuite, Matchers}
 
-class GettingStartedTests extends WordSpec with Matchers {
+class GettingStartedTests extends FunSuite with Matchers {
 
-  "PolymorphicFunctions" must {
-    "isSorted returns true for sorted array" in {
+    test("PolymorphicFunctions.isSorted returns true for sorted array"){
       import scala.math.Ordering
       val as = Array(0, 1, 2, 3, 4)
       PolymorphicFunctions.isSorted(as, Ordering.Int.gt) shouldBe true
     }
-    "isSorted returns false for unsorted array" in {
+    test("PolymorphicFunctions.isSorted returns false for unsorted array") {
       val as = Array(0, 1, 2, 3, 4, 1)
       PolymorphicFunctions.isSorted(as, (a: Int, b: Int) => a > b) shouldBe false
     }
-    "isSorted returns true for one element array" in {
+    test("PolymorphicFunctions.isSorted returns true for one element array") {
       val as = Array(0)
       PolymorphicFunctions.isSorted(as, (a: Int, b: Int) => a > b) shouldBe true
     }
-    "isSorted returns true for empty array" in {
+    test("PolymorphicFunctions.isSorted returns true for empty array") {
       val as = Array[Int]()
       PolymorphicFunctions.isSorted(as, (a: Int, b: Int) => a > b) shouldBe true
     }
-    "curry works properly" in {
+    test("PolymorphicFunctions.curry") {
       val plus: (Int, Int) => Int = (a, b) => a + b
       val plusCurried: Int => Int => Int = PolymorphicFunctions.curry(plus)
       val plus2: Int => Int = (a: Int) => plusCurried(2)(a)
       plus2(3) shouldBe 5
     }
-  }
-
-
+    test("PolymorphicFunctions.uncurry") {
+      val plus: (Int, Int) => Int = (a, b) => a + b
+      val plusCurried: Int => Int => Int = plus.curried
+      val plusUncurried: (Int, Int) => Int = PolymorphicFunctions.uncurry(plusCurried)
+      plusUncurried(1, 2) shouldBe 3
+    }
 
 }
