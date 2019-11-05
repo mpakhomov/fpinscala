fcb68f8f0f9c0674548206184ec47f62c8751d23
mpakhomov <mike.pakhomov@gmail.com>
2019-11-05 23:36:09 +0100

    Tree. Exercise 3.29 fold. Done with Chapter 3 Datastructures

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -17,4 +17,31 @@ object Tree {
     case Branch(l, r) => maximum(l) max maximum(r)
   }
 
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
+
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(x) => Leaf(f(x))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(x) => f(x)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def sizeViaFold[A](t: Tree[A]): Int = fold(t)(_ => 1)(1 + _ + _)
+//     fold(t)(a => 1)((l, r) => 1 + l + r)
+
+  def maximumViaFold(t: Tree[Int]): Int =
+    fold(t)(identity(_))((l, r) => l max r)
+
+  def depthViaFold[A](t: Tree[A]): Int =
+    fold(t)(_ => 0)((l, r) => 1 + (l max r))
+
+  def mapViaFold[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    fold[A, Tree[B]](t)(a => Leaf(f(a)))((l, r) => Branch(l, r))
+//      fold(t)(a => Leaf(f(a)): Tree[B])((l, r) => Branch(l, r))
 }
--- exercises/src/test/scala/datastructures/TreeSpec.scala
+++ exercises/src/test/scala/datastructures/TreeSpec.scala
@@ -10,20 +10,58 @@ class TreeSpec extends WordSpec with Matchers {
                     Branch(Leaf("a"), Leaf("b")),
                     Branch(Leaf("c"), Leaf("d")))
 
+  val treeCharsDepth3 = Branch(
+                          Branch(Leaf("a"), Leaf("b")),
+                          Branch(Leaf("c"), Branch(
+                                              Leaf("d"), Leaf("e"))))
+
   val treeNumbers = Branch(
-    Branch(Leaf(1), Leaf(2)),
-    Branch(Leaf(3), Leaf(4)))
+                      Branch(Leaf(1), Leaf(2)),
+                      Branch(Leaf(3), Leaf(4)))
 
   "Tree" when {
     "size function is used" must {
       "count the number of nodes in a tree" in {
         Tree.size(treeChars) shouldEqual  7
+        Tree.sizeViaFold(treeChars) shouldEqual  7
       }
     }
 
     "maximum function is used" must {
       "find the max element in a tree" in {
         Tree.maximum(treeNumbers) shouldEqual  4
+        Tree.maximumViaFold(treeNumbers) shouldEqual  4
+      }
+    }
+
+    "depth function is used" must {
+      "find the maximum path length from the root of a tree to any leaf" in {
+        Tree.depth(treeChars) shouldBe 2
+        Tree.depthViaFold(treeChars) shouldBe 2
+        Tree.depth(treeNumbers) shouldBe 2
+        Tree.depthViaFold(treeNumbers) shouldBe 2
+        Tree.depth(treeCharsDepth3) shouldBe 3
+        Tree.depthViaFold(treeCharsDepth3) shouldBe 3
+        Tree.depth(Leaf(1)) shouldBe 0
+        Tree.depthViaFold(Leaf(1)) shouldBe 0
+      }
+    }
+
+    "map function is used" must {
+      "modify each element in a tree with a given function" in {
+        val treeCharsUppercase = Branch(
+          Branch(Leaf("A"), Leaf("B")),
+          Branch(Leaf("C"), Leaf("D")))
+        Tree.map(treeChars)(_.toUpperCase) shouldBe treeCharsUppercase
+
+        val treeNumbers1 = Branch(
+                            Branch(Leaf(2), Leaf(3)),
+                            Branch(Leaf(4), Leaf(5)))
+        Tree.map(treeNumbers)(_ + 1) shouldBe treeNumbers1
+        Tree.mapViaFold(treeNumbers)(_ + 1) shouldBe treeNumbers1
+
+        Tree.map(Leaf(1))(identity(_)) shouldBe Leaf(1)
+        Tree.mapViaFold(Leaf(1))(identity(_)) shouldBe Leaf(1)
       }
     }
   }
